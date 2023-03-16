1f68ff6ea3d0defbc0088e71fa7edfb01628eeb4
MeteSiber <127904912+MeteSiber@users.noreply.github.com>
2023-03-16 21:29:58 +0300

    Add files via upload

---
+++ Sıra.py
@@ -0,0 +1,10 @@
+import random
+from time import sleep
+sıra = input("Lütfen Sira Çekmek İçin Enter'e Basin 》")
+while True:
+	if sıra == "":
+		sayi = random.randint(0, 200)
+		sleep(1.3)
+		print("Siradasınız Siranız Şuanda",sayi)
+		sleep(1.3)
+		input("Lütfen Sira Çekmek İçin Enter'e Basin 》")
